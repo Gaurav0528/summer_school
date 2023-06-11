5359d21bc04aacd4d97192778f318b5ebdd16f50
Gaurav0528 <ggupta4_be20@thapar.edu>
2023-06-11 16:25:03 +0530

    Create wrong_output.cpp

---
+++ wrong_output.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+
+    for (int i = 0; i < 5; i++)
+    {
+        a++;
+        b++;
+  
+    }
+
+    int mul = a*b;
+
+    if (mul >= 100 && b < 20); 
+    {
+        cout << "Both conditions are true." << endl;
+    }
+
+    return 0;
+}
