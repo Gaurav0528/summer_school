6d3f0711c677d0aab234b9e65ceec055a82d975c
Gaurav0528 <ggupta4_be20@thapar.edu>
2023-06-14 13:46:26 +0530

    commit

--- debug_wrong_output.cpp
+++ debug_wrong_output.cpp
@@ -6,16 +6,16 @@ int main()
     int a = 5;
     int b = 10;
 
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++){
         a++;
         b++;
-  
     }
-
+    
+    // cout<<"a = "<<a<<" b = "<<b<<endl;;
     int mul = a*b;
-
-    if (mul >= 100 && b < 20);{
+    // cout<<"a*b = "<<mul<<endl;
+    
+    if (mul < 100 && b < 20);{
         cout << "Both conditions are true." << endl;
     }
 
--- factorial_debug.cpp
+++ factorial_debug.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int factorial(int n) {
+    
     int result = 1;
     for (int i = 2; i <= n; i++) {
         result *= i;
