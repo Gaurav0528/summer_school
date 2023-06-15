8469a9ab004ec4e4989bf982a0d1dcd4ba63415c
Gaurav0528 <73791921+Gaurav0528@users.noreply.github.com>
2023-06-16 00:12:18 +0530

    Update 3.sum_debug.cpp

--- 3.sum_debug.cpp
+++ 3.sum_debug.cpp
@@ -19,14 +19,14 @@ int main() {
     bool isGreater = (a > d);
     d = a + c;
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 3; i++) {
         a++;
         b--;
         c *= 2;
         d += 3;
     }
 
-    int sum = calculateSum(a, d);
+    int sum = calculateSum(1, 4);
 
     if (sum >= 100 && d < 20) {
         cout << "Both conditions are true." <<endl;
