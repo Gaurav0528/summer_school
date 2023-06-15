5d5eb53d97b13c28480f5a9ed47877b0c22a2a24
Gaurav0528 <ggupta4_be20@thapar.edu>
2023-06-15 21:11:11 +0530

    Typecasting

---
+++ 6.TypeCasting.cpp
@@ -0,0 +1,22 @@
+// C program to illustrate the use of
+// typecasting
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x=2147483647 ;
+	int sum=x+1;
+	cout<<sum<<endl;
+	
+// 	int a = 15, b = 2; 
+// 	float div;
+// 	div = a / b;
+// 	cout<<div<<endl;
+
+    // int c=1e5,d=1e5;
+    // long long mul=c*d;
+    // cout<<mul;
+	
+	return 0;
+}
